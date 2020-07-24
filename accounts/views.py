0535d9700590d2049b68df21167e8a9eddecc929
todi-2000 <manshitodi@gmail.com>
2020-07-24 23:19:15 +0530

    Added Login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def signup(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        confirmpass=request.POST['confirmpassword']
+        if password==confirmpass:
+            try:
+                user=User.objects.get(username=username)
+                return redirect('signup')
+            except User.DoesNotExist:
+                user=User.objects.create_user(username=username,password=password)
+                auth.login(request,user)
+                return redirect('Home')
+        else:
+            return redirect('signup')
+    return render(request,'accounts/index.html')
+
+def login(request):
+    if request.method=='POST':
+        user=User.objects.get(username=request.POST['username'])
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            return redirect('login')
+    return render(request,'accounts/login.html')
+
+@login_required(login_url='login')
+def home(request):
+    return render(request,'accounts/home.html')
